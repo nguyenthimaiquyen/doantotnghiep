d590b252fc39542c154b327e2ba0f5a3d1b9422c
maiquyen2403 <132974222+nguyenthimaiquyen@users.noreply.github.com>
2024-03-15 16:12:04 +0700

    update

--- doantotnghiep/src/main/java/com/quyen/hust/repository/course/LessonUrlJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.quyen.hust.repository.course;
-
-import com.quyen.hust.entity.course.LessonUrl;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LessonUrlJpaRepository extends JpaRepository<LessonUrl, Long> {
-
-}
